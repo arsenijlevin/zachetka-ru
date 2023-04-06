4cc0a25bb76a87be07c778516b3eb4f26629162a
arsenijlevin <arsenij.levin19@gmail.com>
2023-04-06 21:43:32 +0300

    Добавление и настройка swagger

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle(`API веб-приложения "Зачётка.ру"`)
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(5001);
 }
 bootstrap();
