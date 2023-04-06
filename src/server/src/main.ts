8c64a339401eca9c85d9e2660a337d2bedc68a2d
arsenijlevin <arsenij.levin19@gmail.com>
2023-04-06 21:51:25 +0300

    Префикс /api для всех эндпоинтов

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
