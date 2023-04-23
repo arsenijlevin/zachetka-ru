eed100ba51c065539d3a0a302dc72588cb853c2b
Fyodor <mlgswagdab@gmail.com>
2023-04-23 18:53:10 +0300

    Реализация POST формы

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+  const[grade, setGrade] = React.useState("");
+  const[name, setName] = React.useState("");
   const[text, setText] = React.useState("");
 
-  async function handleClick() {
-    const FormData = {
-      someNumber: 111,
-      someText: "aaa"
-    }
 
+  async function handleClick() {
+    
+    const FormData = {
+      someNumber: parseInt(grade),
+      someText: name
+    }
     const response = await axios.post('http://localhost:5001/api/testing/test-post-with-data', FormData);
     const data = await response.data;
-
+    
     setText(data);
   }
 
   return (
     <>
+      <input type='number' min={0} max={100}  onChange={e => setGrade(e.target.value)}/>
+      <input type='text' onChange={e => setName(e.target.value)}/>
       <button onClick={handleClick}>кнопка</button>
       <p>{text}</p>
     </>
