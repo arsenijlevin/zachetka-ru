6af204617db77751a4ab6137ddc1fb3047ed16f2
Fyodor <mlgswagdab@gmail.com>
2023-04-30 17:37:25 +0300

    Pop-up "Добавить группу"

---
+++ src/client/src/components/pop-ups/AddGroupPopUp.tsx
@@ -0,0 +1,44 @@
+import { Modal, Box } from '@mui/material';
+import { AiOutlineClose } from 'react-icons/ai';
+
+interface AddGroupPopUpProps {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+}
+
+function AddGroupPopUp({ open, setOpen }: AddGroupPopUpProps) {
+
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Modal
+      open={open}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white translate-y-1/3">
+        <button className="self-end">
+          <AiOutlineClose onClick={handleClose} />
+        </button>
+        {/* кнопка закрытия */}
+        <div className="flex flex-col gap-5 self-center">
+          <div>
+            <h4 className="font-bold text-blue-950">Введите код группы</h4>
+            <input type="number" className="mt-2 border border-grey" />
+            {/* Ввод код группы*/}
+          </div>
+          <div>
+            <h4 className="font-bold text-blue-950">Введите семестр обучения</h4>
+            <input type="number" className="mt-2 border border-grey" />
+            {/* Ввод семестра обучения*/}
+          </div>
+          <button className="self-center border border-gray px-2 py-1 bg-gray-100">
+            Применить
+          </button>
+        </div>
+      </Box>
+    </Modal>
+  );
+}
+
+export default AddGroupPopUp;
