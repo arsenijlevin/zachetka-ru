21878ea44279798057c0c5d3508331d321962178
arsenijlevin <arsenij.levin19@gmail.com>
2023-04-30 18:32:00 +0300

    Компонент окна "Изменить дисциплину"

--- src/client/components/pop-ups/ChangeSubjectPopUp.tsx
+++ src/client/components/pop-ups/ChangeSubjectPopUp.tsx
@@ -1,61 +1,102 @@
+import { Modal, Box } from '@mui/material';
 import { AiOutlineClose } from 'react-icons/ai';
-import Select from 'react-select'
+import Select from 'react-select';
+import React from 'react';
 
-function ChangeSubjectPopUp(/* closePopUp: () => void*/) {
+interface ChangeSubjectPopUpProps {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+}
 
-    const subjects = [
-      { value: 'ТП', label: 'ТП' },
-      { value: 'ИПС', label: 'ИПС' },
-      { value: 'ИСиС', label: 'ИСиС' }
-    ]
+function ChangeSubjectPopUp({ open, setOpen }: ChangeSubjectPopUpProps) {
 
-    const lecturers = [
-        { value: 'Преподаватель 1', label: 'Преподаватель 1' },
-        { value: 'Преподаватель 2', label: 'Преподаватель 2' },
-        { value: 'Преподаватель 3', label: 'Преподаватель 3' }
-    ]
+  const handleClose = () => setOpen(false);
 
-    const groups = [
-        { value: '1', label: '1' },
-        { value: '2', label: '2' },
-        { value: '3', label: '3' }
-    ]
+  const subjects = [
+    { value: 'ТП', label: 'ТП' },
+    { value: 'ИПС', label: 'ИПС' },
+    { value: 'ИСиС', label: 'ИСиС' },
+  ];
 
-    const examType = [
-        { value: 'Экзамен', label: 'Экзамен' },
-        { value: 'Зачёт', label: 'Зачёт' }
-    ]
+  const lecturers = [
+    { value: 'Преподаватель 1', label: 'Преподаватель 1' },
+    { value: 'Преподаватель 2', label: 'Преподаватель 2' },
+    { value: 'Преподаватель 3', label: 'Преподаватель 3' },
+  ];
+
+  const groups = [
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+  ];
+
+  const examType = [
+    { value: 'Экзамен', label: 'Экзамен' },
+    { value: 'Зачёт', label: 'Зачёт' },
+  ];
 
   return (
-    <div className='m-auto w-1/2 flex flex-col border border-grey px-16 py-8'>
-      <button className='self-end'>
-        <AiOutlineClose onClick={() => {/* closePopUp*/}}/>
-        </button>{/* кнопка закрытия*/}
-      <div className='flex flex-col gap-5'>
-        <div>
-            <h4 className='font-bold text-blue-950'>Выбор семестра</h4>
-            <input type='number' className='mt-2 border border-grey'/>{/* Ввыбор семестра*/}
+    <Modal
+      open={open}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white translate-y-1/3">
+        <button className="self-end">
+          <AiOutlineClose onClick={handleClose} />
+        </button>
+        {/* кнопка закрытия */}
+        <div className="flex flex-col gap-5">
+          <div>
+            <h4 className="font-bold text-blue-950">Выбор семестра</h4>
+            <input type="number" className="mt-2 border border-grey" />
+            {/* Выбор семестра*/}
+          </div>
+          <div>
+            <h4 className="font-bold text-blue-950">Выбор дисциплины</h4>
+            <Select
+              placeholder="Выберите..."
+              options={subjects}
+              className="mt-2"
+            />
+            {/* Выбор дисциплины*/}
+          </div>
+          <div>
+            <h4 className="font-bold text-blue-950">Выбор преподавателей</h4>
+            <Select
+              placeholder="Выберите..."
+              isMulti
+              options={lecturers}
+              className="mt-2"
+            />
+            {/* Выбор преподавателей*/}
+          </div>
+          <div>
+            <h4 className="font-bold text-blue-950">Выбор группы</h4>
+            <Select
+              placeholder="Выберите..."
+              isMulti
+              options={groups}
+              className="mt-2"
+            />
+            {/* Выбор группы*/}
+          </div>
+          <div>
+            <h4 className="font-bold text-blue-950">Выбор отчётности</h4>
+            <Select
+              placeholder="Выберите..."
+              options={examType}
+              className="mt-2"
+            />
+            {/* Выбор отчётности*/}
+          </div>
+          <button className="self-center border border-gray px-2 py-1 bg-gray-100">
+            Применить
+          </button>
         </div>
-        <div>
-            <h4 className='font-bold text-blue-950'>Выбор дисциплины</h4>
-            <Select placeholder='Выберите...' options={subjects} className='mt-2'/>{/* Выбор дисциплины*/}
-        </div>
-        <div>
-            <h4 className='font-bold text-blue-950'>Выбор преподавателей</h4>
-            <Select placeholder='Выберите...' isMulti options={lecturers} className='mt-2'/>{/* Выбор преподавателей*/}
-        </div>
-        <div>
-            <h4 className='font-bold text-blue-950'>Выбор группы</h4>
-            <Select placeholder='Выберите...' isMulti options={groups} className='mt-2'/>{/* Выбор группы*/}
-        </div>
-        <div>
-            <h4 className='font-bold text-blue-950'>Выбор отчётности</h4>
-            <Select placeholder='Выберите...' options={examType} className='mt-2'/>{/* Выбор отчётности*/}
-        </div>
-        <button className='self-center border border-gray px-2 py-1 bg-gray-100'>Применить</button>
-      </div>
-    </div>
-  )
+      </Box>
+    </Modal>
+  );
 }
 
 export default ChangeSubjectPopUp;
--- src/client/pages/App.tsx
+++ src/client/pages/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import ChangeSubjectPopUp from '../components/pop-ups/ChangeSubjectPopUp';
-import { Modal, Box } from '@mui/material';
-import { AiOutlineClose } from 'react-icons/ai';
-import Select from 'react-select';
 
 function App() {
   const [grade, setGrade] = React.useState('');
@@ -11,32 +8,7 @@ function App() {
   const [text, setText] = React.useState('');
   const [isPopupOpen, setIsPopupOpen] = React.useState(false);
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const subjects = [
-    { value: 'ТП', label: 'ТП' },
-    { value: 'ИПС', label: 'ИПС' },
-    { value: 'ИСиС', label: 'ИСиС' },
-  ];
-
-  const lecturers = [
-    { value: 'Преподаватель 1', label: 'Преподаватель 1' },
-    { value: 'Преподаватель 2', label: 'Преподаватель 2' },
-    { value: 'Преподаватель 3', label: 'Преподаватель 3' },
-  ];
-
-  const groups = [
-    { value: '1', label: '1' },
-    { value: '2', label: '2' },
-    { value: '3', label: '3' },
-  ];
-
-  const examType = [
-    { value: 'Экзамен', label: 'Экзамен' },
-    { value: 'Зачёт', label: 'Зачёт' },
-  ];
+  const handleOpen = () => setIsPopupOpen(true);
 
   async function handleClick() {
     const FormData = {
@@ -85,68 +57,8 @@ function App() {
           Изменить дисциплину
         </button>
       </div>
-      <Modal
-        open={open}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white translate-y-1/3">
-          <button className="self-end">
-            <AiOutlineClose onClick={handleClose} />
-          </button>
-          {/* кнопка закрытия */}
-          <div className="flex flex-col gap-5">
-            <div>
-              <h4 className="font-bold text-blue-950">Выбор семестра</h4>
-              <input type="number" className="mt-2 border border-grey" />
-              {/* Выбор семестра*/}
-            </div>
-            <div>
-              <h4 className="font-bold text-blue-950">Выбор дисциплины</h4>
-              <Select
-                placeholder="Выберите..."
-                options={subjects}
-                className="mt-2"
-              />
-              {/* Выбор дисциплины*/}
-            </div>
-            <div>
-              <h4 className="font-bold text-blue-950">Выбор преподавателей</h4>
-              <Select
-                placeholder="Выберите..."
-                isMulti
-                options={lecturers}
-                className="mt-2"
-              />
-              {/* Выбор преподавателей*/}
-            </div>
-            <div>
-              <h4 className="font-bold text-blue-950">Выбор группы</h4>
-              <Select
-                placeholder="Выберите..."
-                isMulti
-                options={groups}
-                className="mt-2"
-              />
-              {/* Выбор группы*/}
-            </div>
-            <div>
-              <h4 className="font-bold text-blue-950">Выбор отчётности</h4>
-              <Select
-                placeholder="Выберите..."
-                options={examType}
-                className="mt-2"
-              />
-              {/* Выбор отчётности*/}
-            </div>
-            <button className="self-center border border-gray px-2 py-1 bg-gray-100">
-              Применить
-            </button>
-          </div>
-        </Box>
-      </Modal>
 
-      {isPopupOpen ? <ChangeSubjectPopUp /> : <></>}
+      {isPopupOpen ? <ChangeSubjectPopUp open={isPopupOpen} setOpen={setIsPopupOpen}/> : <></>}
     </>
   );
 }
