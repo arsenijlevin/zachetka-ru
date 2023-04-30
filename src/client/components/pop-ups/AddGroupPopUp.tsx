187ca952185b73c18b8b4e93c19efe6d4dcaf6f8
arsenijlevin <arsenij.levin19@gmail.com>
2023-04-30 19:50:27 +0300

    MaterialUI для элементов формы

--- src/client/components/pop-ups/AddGroupPopUp.tsx
+++ src/client/components/pop-ups/AddGroupPopUp.tsx
@@ -1,4 +1,4 @@
-import { Modal, Box, Typography } from '@mui/material';
+import { Modal, Box, Input, Button, Typography } from '@mui/material';
 import { AiOutlineClose } from 'react-icons/ai';
 
 interface AddGroupPopUpProps {
@@ -7,7 +7,6 @@ interface AddGroupPopUpProps {
 }
 
 function AddGroupPopUp({ open, setOpen }: AddGroupPopUpProps) {
-
   const handleClose = () => setOpen(0);
 
   return (
@@ -17,26 +16,40 @@ function AddGroupPopUp({ open, setOpen }: AddGroupPopUpProps) {
       aria-describedby="modal-modal-description"
     >
       <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white translate-y-1/3">
-      <button className="self-end">
+        {/* Кнопка закрытия */}
+        <button className="self-end">
           <AiOutlineClose onClick={handleClose} />
         </button>
-        <h2 className='self-center font-bold text-2xl pb-8'>Добавить группу</h2>
-        {/* кнопка закрытия */}
-        <div className="flex flex-col gap-5 self-center">
-          <div>
-            <h4 className="font-bold text-blue-950">Введите код группы</h4>
-            <input type="number" className="mt-2 border border-grey" />
-            {/* Ввод код группы*/}
-          </div>
-          <div>
-            <h4 className="font-bold text-blue-950">Введите семестр обучения</h4>
-            <input type="number" className="mt-2 border border-grey" />
-            {/* Ввод семестра обучения*/}
-          </div>
-          <button className="self-center border border-gray px-2 py-1 bg-gray-100">
+
+        <Box className="flex flex-col gap-8 self-center">
+          <Typography variant="h5" fontWeight="bold">
+            Добавить группу
+          </Typography>
+
+          {/* Ввод код группы*/}
+          <Box>
+            <Typography variant="body1">Введите код группы</Typography>
+            <Input className="mt-2 p-1" />
+          </Box>
+
+          {/* Ввод семестра обучения*/}
+          <Box>
+            <Typography variant="body1">Введите семестр</Typography>
+            <Input
+              type="number"
+              fullWidth
+              className="mt-2 p-1"
+              inputProps={{
+                min: 1,
+                max: 12,
+              }}
+            />
+          </Box>
+
+          <Button variant="contained" size="medium" className="px-2 py-1">
             Добавить
-          </button>
-        </div>
+          </Button>
+        </Box>
       </Box>
     </Modal>
   );
