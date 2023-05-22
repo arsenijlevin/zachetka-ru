1116c11acc46b6daf02e7c63b104bd355f66a15b
arsenijlevin <arsenij.levin19@gmail.com>
2023-05-22 21:25:14 +0300

    Исправление ошибки импорта

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestingModule } from './testing/testing.module';
-import { UsersModule } from './Users/Users.module';
+import { UsersModule } from './Users/users.module';
 
 @Module({
   imports: [TestingModule, UsersModule],
