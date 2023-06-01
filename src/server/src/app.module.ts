bbfd3b015498e8bfa0b436b353d0f0aef4353162
arsenijlevin <arsenij.levin19@gmail.com>
2023-06-02 00:12:21 +0300

    Основа модуля авторизации

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TestingModule } from './testing/testing.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 export const moduleSettings = {
-  imports: [TestingModule, UsersModule, ConfigModule.forRoot()]
+  imports: [TestingModule, UsersModule, ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
 };
 
 @Module(moduleSettings)
---
+++ src/server/src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) { }
+
+  async signIn(login: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne({
+      login,
+    });
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
+  }
+}
