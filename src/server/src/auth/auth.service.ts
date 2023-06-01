acf20a5f77eeaaec45e46472612360d5c3da4d15
arsenijlevin <arsenij.levin19@gmail.com>
2023-06-02 00:17:09 +0300

    Создание guard класса

--- src/server/src/auth/auth.service.ts
+++ src/server/src/auth/auth.service.ts
@@ -1,20 +1,27 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+export interface AuthUser {
+  login: string;
+  username: string;
+}
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) { }
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) { }
 
-  async signIn(login: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne({
-      login,
-    });
+  async signIn(login: string, pass: string) {
+    const user = await this.usersService.findOne({ login });
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    const payload: AuthUser = { login: user.login, username: user.name };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
