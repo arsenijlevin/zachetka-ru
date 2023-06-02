a9b94387499238c88cd540ca4718dd37c882307b
arsenijlevin <arsenij.levin19@gmail.com>
2023-06-02 20:14:44 +0300

    Swagger без @ApiProperty

--- src/server/src/users/dto/create-user.dto.ts
+++ src/server/src/users/dto/create-user.dto.ts
@@ -1,17 +1,16 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 
 export class CreateUserDto {
   @IsString()
-  @ApiProperty()
+
   login: string;
   @IsString()
-  @ApiProperty()
+
   name: string;
   @IsString()
-  @ApiProperty()
+
   password: string;
   @IsNumber()
-  @ApiProperty()
+
   rights_id: number;
 }
