411dfb532b86f540bb9f5cc52a7d07f51494b911
Fyodor <mlgswagdab@gmail.com>
2023-06-02 17:51:43 +0300

    Таблицы посещаемости и успеваемости переведены на DataGrid

--- src/client/components/tables/AttendanceTable.tsx
+++ src/client/components/tables/AttendanceTable.tsx
@@ -1,6 +1,8 @@
-import { Box, Breadcrumbs, Button, Table, Typography } from '@mui/material';
+import { Box, Breadcrumbs, Button, Typography } from '@mui/material';
+import { DataGrid, GridRowsProp, GridColDef, GridRowModel } from '@mui/x-data-grid';
 import * as React from 'react';
 import Link from 'next/link';
+import { useCallback } from 'react';
 
 // function createData(
 //   name: string,
@@ -16,7 +18,7 @@ import Link from 'next/link';
 const subject = 'Технологии программирования (6 семестр)';
 const group = 'Группа 1';
 
-function createData(
+/* function createData(
   name: string,
   attendance: string[],
   n: number,
@@ -25,27 +27,100 @@ function createData(
   sum: number,
 ) {
   return { name, attendance, n, p, b, sum };
-}
+} */
 
-const dates = [
-  '1.12\n15:00', '2.12\n8:00', '6.12\n8:00', '7.12\n8:00', '7.12\n8:00', '10.12\n15:00', '11.12\n8:00', '20.12\n8:00', '21.12\n8:00', '22.12\n8:00',
-  '1.12\n15:00', '2.12\n8:00', '6.12\n8:00', '7.12\n8:00', '7.12\n8:00', '10.12\n15:00', '11.12\n8:00', '20.12\n8:00', '21.12\n8:00', '22.12\n8:00'
-]
-
-const rows = [
-  createData('Иванов А. А.', ['', 'Н', '', '', '', '', 'Н', '', '', '', 'П', '', 'П', '', 'Б', 'П', '', 'П', '', 'Б'], 0, 0, 0, 1),
-  createData('Сидоров П. С.', ['', 'Б', '', 'Б', 'Б', '', 'Н', '', '', '', 'П', '', 'П', '', 'Б', 'П', '', 'П', '', 'Б'], 0, 0, 0, 2),
-  createData('Тереньтев Г. Г.', ['', '', 'Н', 'Б', 'Б', 'П', '', 'П', '', 'Б', 'П', '', 'П', '', 'Б', 'П', '', 'П', '', 'Б'], 0, 0, 0, 3),
-  createData('Адреев Б. Б.', ['П', '', 'П', '', 'Б', 'П', '', 'П', '', 'Б', 'П', '', 'П', '', 'Б', 'П', '', 'П', '', 'Б'], 0, 0, 0, 4),
+const rows: GridRowsProp = [
+  { id: 1, col2: '', col3: 'Н', col4: '', col5: '', col6: '', col7: '', col8: 'Н', col9: '', col10: '', col11: '', col12: 'П', col13: '', col14: 'П', col15: '', col16: 'Б', col17: 'П', col18: '', col19: 'П', col20: '', col21: 'Б', col22: 2, col23: 4, col24: 2, col25: 8 },
+  { id: 2, col2: '', col3: 'Н', col4: '', col5: '', col6: '', col7: '', col8: 'Н', col9: '', col10: '', col11: '', col12: 'П', col13: '', col14: 'П', col15: '', col16: 'Б', col17: 'П', col18: '', col19: 'П', col20: '', col21: 'Б', col22: 2, col23: 4, col24: 2, col25: 8 },
+  { id: 3, col2: '', col3: 'Н', col4: '', col5: '', col6: '', col7: '', col8: 'Н', col9: '', col10: '', col11: '', col12: 'П', col13: '', col14: 'П', col15: '', col16: 'Б', col17: 'П', col18: '', col19: 'П', col20: '', col21: 'Б', col22: 2, col23: 4, col24: 2, col25: 8 },
+  { id: 4, col2: '', col3: 'Н', col4: '', col5: '', col6: '', col7: '', col8: 'Н', col9: '', col10: '', col11: '', col12: 'П', col13: '', col14: 'П', col15: '', col16: 'Б', col17: 'П', col18: '', col19: 'П', col20: '', col21: 'Б', col22: 2, col23: 4, col24: 2, col25: 8 },
 ];
 
+const columns: GridColDef[] = [
+  { field: 'col2', headerName: '1.12\n15:00', width: 100, editable: true },
+  { field: 'col3', headerName: '2.12\n8:00', width: 100, editable: true },
+  { field: 'col4', headerName: '6.12\n8:00', width: 100, editable: true },
+  { field: 'col5', headerName: '7.12\n8:00', width: 100, editable: true },
+  { field: 'col6', headerName: '7.12\n8:00', width: 100, editable: true },
+  { field: 'col7', headerName: '10.12\n15:00', width: 100, editable: true },
+  { field: 'col8', headerName: '11.12\n8:00', width: 100, editable: true },
+  { field: 'col9', headerName: '20.12\n8:00', width: 100, editable: true },
+  { field: 'col10', headerName: '21.12\n8:00', width: 100, editable: true },
+  { field: 'col11', headerName: '22.12\n8:00', width: 100, editable: true },
+  { field: 'col12', headerName: '1.12\n15:00', width: 100, editable: true },
+  { field: 'col13', headerName: '2.12\n8:00', width: 100, editable: true },
+  { field: 'col14', headerName: '6.12\n8:00', width: 100, editable: true },
+  { field: 'col15', headerName: '7.12\n8:00', width: 100, editable: true },
+  { field: 'col16', headerName: '7.12\n8:00', width: 100, editable: true },
+  { field: 'col17', headerName: '10.12\n15:00', width: 100, editable: true },
+  { field: 'col18', headerName: '11.12\n8:00', width: 100, editable: true },
+  { field: 'col19', headerName: '20.12\n8:00', width: 100, editable: true },
+  { field: 'col20', headerName: '21.12\n8:00', width: 100, editable: true },
+  { field: 'col21', headerName: '22.12\n8:00', width: 100, editable: true },
+];
+
+const firstRows: GridRowsProp = [
+  { id: 1, col1: 'Иванов А. А.' },
+  { id: 2, col1: 'Иванов А. А.' },
+  { id: 3, col1: 'Иванов А. А.' },
+  { id: 4, col1: 'Иванов А. А.' },
+];
+
+const firstColumns: GridColDef[] = [
+  { field: 'col1', headerName: 'ФИО студента', width: 150 },
+];
+
+const lastRows: GridRowsProp = [
+  { id: 1, col1: 2, col2: 4, col3: 2, col4: 8 },
+  { id: 2, col1: 3, col2: 4, col3: 2, col4: 8 },
+  { id: 3, col1: 1, col2: 4, col3: 2, col4: 8 },
+  { id: 4, col1: 0, col2: 4, col3: 2, col4: 8 },
+];
+
+const lastColumns: GridColDef[] = [
+  { field: 'col1', headerName: 'Н', width: 50 },
+  { field: 'col2', headerName: 'П', width: 50 },
+  { field: 'col3', headerName: 'Б', width: 50 },
+  { field: 'col4', headerName: 'Сумма \n пропусков', width: 150 },
+];
+
+
+
+/* const useFakeMutation = () => {
+  return React.useCallback(
+    (user: Partial<User>) =>
+      new Promise<Partial<User>>((resolve, reject) => {
+        setTimeout(() => {
+          if (user.name?.trim() === '') {
+            reject(new Error("Error while saving user: name can't be empty."));
+          } else {
+            resolve({ ...user, name: user.name?.toUpperCase() });
+          }
+        }, 200);
+      }),
+    [],
+  );
+};
+*/
+
 export default function AttendanceTable() {
+  const processRowUpdate = useCallback(
+    (newRow: GridRowModel, oldRow: GridRowModel) => {
+      // Make the HTTP request to save in the backend
+      // const response = await mutateRow(newRow);
+      console.log(newRow);
+      return newRow;
+    },
+    [],
+  );
+
   return (
     <Box sx={{ width: '2000px' }}>
       <Typography variant='h3' position={'sticky'} left={'5%'} width={'500px'}>Учёт посещаемости</Typography>
       <Link href='grades-table' passHref>
-        <Button variant='outlined' style={{ width: '200px', position: 'fixed', right: 0 }}>Открыть оценки</Button>
+        <Button variant='outlined' style={{ width: '200px', position: 'fixed', right: '5%' }}>Открыть оценки</Button>
       </Link>
+      <br />
       <Box sx={{ position: 'sticky', left: '5%', width: '600px' }}>
         <Breadcrumbs aria-label="breadcrumb">
           <Link className='underline hover:underline-offset-1 hover:text-blue-700 text-blue-500' href='subjects-list'>{subject}</Link>
@@ -54,96 +129,37 @@ export default function AttendanceTable() {
         </Breadcrumbs>
       </Box>
       <br />
-      <Typography variant='h5' style={{ width: '400px', position: 'sticky', left: '5%' }}>Общее количество занятий: {dates.length}</Typography>
+      <Typography variant='h5' style={{ width: '400px', position: 'sticky', left: '5%' }}>Общее количество занятий: 20</Typography>
       <br />
-      <Table
-        sx={{
-          '& tr > *:first-child': {
-            border: '1px solid black',
-            position: 'sticky',
-            left: 0,
-            backgroundColor: '#e0e0e0',
-          },
-          'td': {
-            border: '1px solid black',
-            padding: '10px',
-            background: 'white',
-          },
-          'tbody td.stuck': {
-            position: 'sticky',
-            right: 0,
-            backgroundColor: 'white',
-          },
-          'tr td:nth-last-child(1)': {
-            position: 'sticky',
-            right: 0,
-            backgroundColor: '#e0e0e0',
-          },
-          'tr td:nth-last-child(2)': {
-            position: 'sticky',
-            right: 120,
-            backgroundColor: '#e0e0e0',
-          },
-          'tr td:nth-last-child(3)': {
-            position: 'sticky',
-            right: 220,
-            backgroundColor: '#e0e0e0',
-          },
-          'tr td:nth-last-child(4)': {
-            position: 'sticky',
-            right: 320,
-            backgroundColor: '#e0e0e0',
-          },
-          'tr th:nth-last-child(1),': {
-            position: 'sticky',
-            right: 0,
-            backgroundColor: '#e0e0e0',
-            textAlign: 'left',
-          },
-          'tr th:nth-last-child(2),': {
-            position: 'sticky',
-            right: 120,
-            backgroundColor: '#e0e0e0',
-          },
-          'tr th:nth-last-child(3),': {
-            position: 'sticky',
-            right: 220,
-            backgroundColor: '#e0e0e0',
-          },
-          'tr th:nth-last-child(4),': {
-            position: 'sticky',
-            right: 320,
-            backgroundColor: '#e0e0e0',
-          }
-        }}
-      >
-        <thead>
-          <tr>
-            <th style={{ width: 100 }}>Ф.И.О. студента</th>
-            {dates.map((date) => (
-                <th style={{ width: 50 }}>{date}</th>
-              ))}
-            <th style={{ width: 100 }}>Н</th>
-            <th style={{ width: 100 }}>П</th>
-            <th style={{ width: 100 }}>Б</th>
-            <th style={{ width: 100 }}>Сумма пропусков</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row) => (
-            <tr key={row.name}>
-              <td>{row.name}</td>
-              {row.attendance.map((a) => (
-                <td>{a}</td>
-              ))}
-              <td className="stuck">{row.n}</td>
-              <td className="stuck">{row.b}</td>
-              <td className="stuck">{row.p}</td>
-              <td className="stuck">{row.sum}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <Box sx={{ position: 'fixed', left: '5%', width: '90%' }}>
+        <Box sx={{ position: 'fixed', left: '5%', width: '10%' }}>
+          <DataGrid rows={firstRows} columns={firstColumns} hideFooter={true}
+          sx={{
+            borderRadius: '0px',
+          }}/>     
+        </Box>
+        <Box sx={{ position: 'fixed', left: '15%', width: '60%' }}>
+          <DataGrid rows={rows} columns={columns} hideFooter={true} processRowUpdate={processRowUpdate}
+          sx={{
+            '.MuiDataGrid-columnHeaderTitle': { 
+              fontWeight: 'bold !important',
+              },
+              "& .MuiDataGrid-cell": {
+                borderRight: 1,
+                borderRightColor: 'rgb(220,220,220)'
+              },
+              borderRadius: '0px',
+              borderLeft: '0px',
+            }}
+            />
+        </Box>
+        <Box sx={{ position: 'fixed', right: '5%', width: '20%' }}>
+          <DataGrid rows={lastRows} columns={lastColumns} hideFooter={true}
+          sx={{
+            borderRadius: '0px',
+          }}/>
+        </Box>
+      </Box>
     </Box>
   );
 }
--- src/client/components/tables/GradesTable.tsx
+++ src/client/components/tables/GradesTable.tsx
@@ -1,28 +1,27 @@
-import { Box, Breadcrumbs, Button, Table, Typography } from '@mui/material';
+import { Box, Breadcrumbs, Button, Typography } from '@mui/material';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import Link from 'next/link';
 
 const subject ='Технологии программирования (6 семестр)';
 const group = 'Группа 1';
 
-function createData(
-    name: string,
-    kt1: number,
-    kt2: number,
-    kt3: number,
-    kta: number,
-    exam: number,
-    sf: number,
-    result: string,
-  ) {
-    return { name, kt1, kt2, kt3, kta, exam, sf, result };
-  }
+const rows: GridRowsProp = [
+  { id: 1, col1: 'Иванов А. А.', col2: 25, col3: 25, col4: 25, col5: 25, col6: 25, col7: 50, col8: 'Удовлетворительно' },
+  { id: 2, col1: 'Сидоров П. С.', col2: 50, col3: 50, col4: 50, col5: 50, col6: 50, col7: 100, col8: 'Отлично' },
+  { id: 3, col1: 'Тереньтев Г. Г.', col2: 40, col3: 40, col4: 40, col5: 40, col6: 40, col7: 80, col8: 'Хорошо' },
+  { id: 4, col1: 'Адреев Б. Б.', col2: 45, col3: 45, col4: 45, col5: 45, col6: 45, col7: 90, col8: 'Отлично' },
+];
 
-  const rows = [
-    createData('Иванов А. А.', 25, 25, 25, 25, 25, 50, 'Удовлетворительно'),
-    createData('Сидоров П. С.', 50, 50, 50, 50, 50, 100, 'Отлично'),
-    createData('Тереньтев Г. Г.', 40, 40, 40, 40, 40, 80, 'Хорошо'),
-    createData('Адреев Б. Б.', 45, 45, 45, 45, 45, 90, 'Отлично'),
-  ];
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'ФИО студента', width: 200  },
+  { field: 'col2', headerName: 'КТ1', width: 150, editable: true },
+  { field: 'col3', headerName: 'КТ2', width: 150, editable: true },
+  { field: 'col4', headerName: 'КТ3', width: 150, editable: true },
+  { field: 'col5', headerName: 'КТ сред.', width: 150 },
+  { field: 'col6', headerName: 'Экзамен', width: 150, editable: true },
+  { field: 'col7', headerName: 'Итоговый балл', width: 150 },
+  { field: 'col8', headerName: 'Итог', width: 200 },
+];
 
 export default function GradesTable() {
     return(
@@ -41,39 +40,11 @@ export default function GradesTable() {
               <Link className='underline hover:underline-offset-1 hover:text-blue-700 text-blue-500' href='attendance-table'>Учёт успеваемости</Link>
             </Breadcrumbs>
             <br/>
-            <Table
-              sx={{ 
-                'td':{
-                  border: '1px solid black',
-                }
-               }}>
-                <thead>
-                  <tr>
-                    <th>ФИО студента</th>
-                    <th>КТ1</th>
-                    <th>КТ2</th>
-                    <th>КТ3</th>
-                    <th>КТ сред.</th>
-                    <th>Экзамен</th>
-                    <th>Итоговый балл</th>
-                    <th>Итог</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {rows.map((row) => (
-                    <tr key={row.name}>
-                      <td>{row.name}</td>
-                      <td>{row.kt1}</td>
-                      <td>{row.kt2}</td>
-                      <td>{row.kt3}</td>
-                      <td>{row.kta}</td>
-                      <td>{row.exam}</td>
-                      <td>{row.sf}</td>
-                      <td>{row.result}</td>
-                    </tr>
-                  ))}
-                </tbody>
-            </Table>
+            <DataGrid rows={rows} columns={columns} sx={{
+              '.MuiDataGrid-columnHeaderTitle': { 
+                fontWeight: 'bold !important',
+              }
+            }}/>
         </Box>
         </>
     );
