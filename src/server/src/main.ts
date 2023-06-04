bdbf9d1d3006e89d0cd1ce25b278c34d3f55de53
arsenijlevin <arsenij.levin19@gmail.com>
2023-06-04 21:26:57 +0300

    Стандартный админ

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import * as bcrypt from 'bcrypt';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -16,6 +17,33 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const defaultAdmin = await prismaService.users.findFirst({
+    where: {
+      login: 'admin_default',
+    },
+  })
+
+  if (!defaultAdmin) {
+    const login = "admin_default";
+    const password = await bcrypt.hash(login, 10);
+
+
+    await prismaService.users.create({
+      data: {
+        login: login,
+        password: password,
+        name: "Администратор",
+        rights_id: 3
+      }
+    })
+
+    setTimeout(() => {
+      console.warn("Администратор не обнаружен! Создан администратор с стандартными данными admin_default!")
+      console.warn("Сразу после запуска измените пароль у администратора admin_default!")
+    }, 5000);
+  }
+
 
   const config = new DocumentBuilder()
     .setTitle(`API веб-приложения "Зачётка.ру"`)
