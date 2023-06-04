5797fba15a61f5e87193a7dc485a636050e29872
Fyodor <mlgswagdab@gmail.com>
2023-06-04 22:21:00 +0300

    Вёрстка проверки посещаемости

--- src/client/components/lists/GroupsList.tsx
+++ src/client/components/lists/GroupsList.tsx
@@ -1,5 +1,5 @@
 import { Box, Breadcrumbs, Button, Typography } from '@mui/material';
-import AttendanceCheckPopUp from 'components/pop-ups/AttendanceCheckPopUp';
+import AttendanceCheckPopUp from '../pop-ups/AttendanceCheckPopUp';
 import Link from 'next/link';
 import React from 'react';
 
@@ -18,21 +18,21 @@ function GroupsList() {
 
     return(
         <>
-        <Box sx={{ position: 'relative', left: '5%', width: '90%' }}>
-            <Typography variant='h3'>Выберите группу</Typography>
-            <Button variant='outlined' style={{ width: '250px', position: 'fixed', right: '5%' }} onClick={handleOpenAttendanceCheck} >Проверка посещаемости</Button>
-            <br />
-            <Breadcrumbs aria-label="breadcrumb">
-                <Link className='underline hover:underline-offset-1 hover:text-blue-700 text-blue-500' href='subjects-list'>{subject}</Link><></>
-            </Breadcrumbs>
-            <br />
-            <Box className="flex flex-col gap-4">
-                {list.map((item) => (
-                    <Link href='attendance-table' passHref style={{ textDecoration: 'underline', color: '#1E90FF', fontSize: '20px' }}>{item}</Link>
-                ))}
+            <Box sx={{ position: 'relative', left: '5%', width: '90%' }}>
+                <Typography variant='h3'>Выберите группу</Typography>
+                <Button variant='outlined' style={{ width: '250px', position: 'fixed', right: '5%' }} onClick={handleOpenAttendanceCheck} >Проверка посещаемости</Button>
+                <br />
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link className='underline hover:underline-offset-1 hover:text-blue-700 text-blue-500' href='subjects-list'>{subject}</Link><></>
+                </Breadcrumbs>
+                <br />
+                <Box className="flex flex-col gap-4">
+                    {list.map((item) => (
+                        <Link href='attendance-table' passHref style={{ textDecoration: 'underline', color: '#1E90FF', fontSize: '20px' }}>{item}</Link>
+                    ))}
+                </Box>
             </Box>
-        </Box>
-        {SelectedModal ? <SelectedModal open={true} setOpen={setIsPopupOpen}/> : <></>}
+            {SelectedModal ? <SelectedModal open={true} setOpen={setIsPopupOpen}/> : <></>}
         </>
     );
 }
--- src/client/components/pop-ups/AttendanceCheckPopUp.tsx
+++ src/client/components/pop-ups/AttendanceCheckPopUp.tsx
@@ -6,17 +6,46 @@ import { MultiSectionDigitalClock } from '@mui/x-date-pickers/MultiSectionDigita
 import { AiOutlineClose } from 'react-icons/ai';
 import React from 'react';
 import Select from 'react-select';
+import Countdown from 'react-countdown';
 
-interface AddGroupPopUpProps {
+interface AttendanceCheckPopUpProps {
     open: boolean;
     setOpen: (value: number) => void;
 }
 
-function AttendanceCheckPopUp({ open, setOpen }: AddGroupPopUpProps) {
-    const[studentsQuantity, setStudentsQuantity] = React.useState(15);
-    const[subjectName, setSubjectName] = React.useState('Технологии программирования');
-    const[semester, setSemester] = React.useState(6);
+interface TimerProps {
+    minutes: number;
+    seconds: number;
+    completed: boolean;
+}
+
+function AttendanceCheckPopUp({ open, setOpen }: AttendanceCheckPopUpProps) {
+    const studentsQuantity = 15;
+    const subjectName = 'Технологии программирования';
+    const semester = 6;
     const handleClose = () => setOpen(0);
+    const[state, setState] = React.useState(0)
+    const code = 'test';
+    const time = 60000;
+    const groups = [
+        {
+            title: '1',
+            studentsChecked: 3
+        },
+        {
+            title: '2',
+            studentsChecked: 7
+        }
+    ];
+    const studentsChecked = 10;
+
+    function handleChangeStateToMiddle() {
+        setState(1);
+    }
+
+    function handleChangeStateToEnd() {
+        setState(2);
+    }
 
     const days = [
         { value: 'Понедельник', label: 'Понедельник' },
@@ -31,52 +60,119 @@ function AttendanceCheckPopUp({ open, setOpen }: AddGroupPopUpProps) {
         { value: 'Знаменатель', label: 'Знаменатель' }
     ];
 
+    const renderer = ({ minutes, seconds, completed }: TimerProps) => {
+        if (completed) {
+          return <Typography variant="h1" textAlign={'center'}>00:00</Typography>;
+        } else {
+          return <Typography variant="h1" textAlign={'center'}>{minutes}:{seconds}</Typography>;
+        }
+      };
 
-    return(
-        <Modal
-        open={open}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        >
-            <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white mt-16">
-                <button className="self-end">
-                    <AiOutlineClose onClick={handleClose} />
-                </button>
-                <Box className="flex flex-col gap-8 self-center">
-                    <Typography variant="h6" textAlign={'center'}>
-                        Настройка проверки посещаемости
-                    </Typography>
-                    <Typography variant="h6" textAlign={'center'}>
-                        {subjectName} ({semester} семестр)
-                    </Typography>
-                    <br />
-                    <Typography variant="h6" textAlign={'center'} fontStyle={'italic'}>
-                        Введите время проверки от 00:30 до 15:00
-                    </Typography>
-                    <LocalizationProvider localeText={ruRU.components.MuiLocalizationProvider.defaultProps.localeText}
-                        dateAdapter={AdapterLuxon} adapterLocale='ru'>
-                            <MultiSectionDigitalClock sx={{ width: '130px', margin: 'auto',
-                            '.css-1916zh5-MuiList-root-MuiMultiSectionDigitalClockSection-root': {
-                                width: '60px',
-                            }
-                        }} timeSteps={{ minutes: 1 }} views={['minutes', 'seconds']} />
-                    </LocalizationProvider>
-                    <Select
-                    placeholder="Выберите день недели"
-                    options={days}/>
-                    <Select
-                    placeholder="Числитель/знаменатель"
-                    options={frequency}/>
-                    <Typography variant="h6" textAlign={'center'}>
-                        На занятии должно присутствовать {studentsQuantity} студентов
-                    </Typography>
-                    <Button variant='outlined' style={{ width: '500px' }}>Запустить проверку посещаемости</Button>
+
+    if (state === 0) {
+        return(
+            <Modal
+            open={open}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+                <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white mt-16">
+                    <button className="self-end">
+                        <AiOutlineClose onClick={handleClose} />
+                    </button>
+                    <Box className="flex flex-col gap-8 self-center">
+                        <Typography variant="h6" textAlign={'center'}>
+                            Настройка проверки посещаемости
+                        </Typography>
+                        <Typography variant="h6" textAlign={'center'}>
+                            {subjectName} ({semester} семестр)
+                        </Typography>
+                        <br />
+                        <Typography variant="h6" textAlign={'center'} fontStyle={'italic'}>
+                            Введите время проверки от 00:30 до 15:00
+                        </Typography>
+                        <LocalizationProvider localeText={ruRU.components.MuiLocalizationProvider.defaultProps.localeText}
+                            dateAdapter={AdapterLuxon} adapterLocale='ru'>
+                                <MultiSectionDigitalClock sx={{ width: '130px', margin: 'auto',
+                                '.css-1916zh5-MuiList-root-MuiMultiSectionDigitalClockSection-root': {
+                                    width: '60px',
+                                }
+                            }} timeSteps={{ minutes: 1 }} views={['minutes', 'seconds']} />
+                        </LocalizationProvider>
+                        <Select
+                        placeholder="Выберите день недели"
+                        options={days}/>
+                        <Select
+                        placeholder="Числитель/знаменатель"
+                        options={frequency}/>
+                        <Typography variant="h6" textAlign={'center'}>
+                            На занятии должно присутствовать {studentsQuantity} студентов
+                        </Typography>
+                        <Button variant='outlined' style={{ width: '500px' }} onClick={handleChangeStateToMiddle}>Запустить проверку посещаемости</Button>
+                    </Box>
                 </Box>
-            </Box>
-        </Modal>
-    );
+            </Modal>
+        );
+    }
+
+    if (state === 1) {
+        return(
+            <Modal
+            open={open}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+                <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white mt-16">
+                    <Box className="flex flex-col gap-8 self-center">
+                        <button className="self-end">
+                            <AiOutlineClose onClick={handleClose} />
+                        </button>
+                        <Typography variant="h6" textAlign={'center'}>
+                            Идёт проверка посещаемости
+                        </Typography>
+                        <Countdown date={Date.now() + time} renderer={renderer} className='text-center'/>
+                        <Typography variant="h6" textAlign={'center'}>
+                            Код: {code}
+                        </Typography>
+                        <Button variant='outlined' style={{ width: '500px' }} onClick={handleChangeStateToEnd}>Закончить проверку посещаемости</Button>
+                    </Box>
+                </Box>
+            </Modal>
+        );
+    }
+
+    if (state === 2) {
+        return(
+            <Modal
+            open={open}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+                <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white mt-16">
+                    <Box className="flex flex-col self-center">
+                        <button className="self-end">
+                            <AiOutlineClose onClick={handleClose} />
+                        </button>
+                        <Typography variant="h6" textAlign={'center'}>
+                            Итог проверки посещаемости
+                        </Typography>
+                        <Typography variant="h1" textAlign={'center'}>
+                            00:00
+                        </Typography>
+                        {groups.map((item) => (
+                            <Typography variant="h6" textAlign={'center'}>Группа {item.title} - студентов отметилось: {item.studentsChecked}</Typography>
+                        ))}
+                        <Typography variant="h6" textAlign={'center'}>Всего студентов на занятии: {studentsChecked}</Typography>
+                        <Typography variant="h6" textAlign={'center'}>
+                            На занятии должно присутствовать {studentsQuantity} студентов
+                        </Typography>
+                        <br />
+                        <Button variant='outlined' style={{ width: '500px' }} onClick={handleClose}>Сбросить учтённую сейчас посещаемость</Button>
+                    </Box>
+                </Box>
+            </Modal>
+        );
+    }
 }
 
 export default AttendanceCheckPopUp;
-
-//день, числитель\знаменатель, время занятия
