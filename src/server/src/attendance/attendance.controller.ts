362f297cc4ea2f045f5e232e6e36df25f3da27c5
arsenijlevin <arsenij.levin19@gmail.com>
2023-09-05 14:04:10 +0300

    Получение посещаемости по группе и предмету

--- src/server/src/attendance/attendance.controller.ts
+++ src/server/src/attendance/attendance.controller.ts
@@ -4,17 +4,27 @@ import { PostAttendanceDto } from './dto/post-attendance.dto';
 import { AttendanceService } from './attendance.service';
 
 @Controller('attendance')
-@ApiTags("Посещаемость")
+@ApiTags('Посещаемость')
 export class AttendanceController {
-  constructor(private readonly attendanceService: AttendanceService) { }
+  constructor(private readonly attendanceService: AttendanceService) {}
 
   @Get('findAllForStudent/:student_login/:subject_id')
-  public findAllForStudent
-    (
-      @Param('student_login') student_login: string,
-      @Param('subject_id') subject_id: number
-    ) {
+  public findAllForStudent(
+    @Param('student_login') student_login: string,
+    @Param('subject_id') subject_id: number,
+  ) {
     return this.attendanceService.findAllForStudent(student_login, +subject_id);
+  }
+
+  @Get('findAllForSubjectGroup/:subject_id/:group_id')
+  public findAllForSubjectGroup(
+    @Param('subject_id') subject_id: number,
+    @Param('group_id') group_id: number,
+  ) {
+    return this.attendanceService.findAllForSubjectGroup(
+      +subject_id,
+      +group_id,
+    );
   }
 
   @Patch('post')
