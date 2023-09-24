2f16c3ac8baf7cc65f90d6bab01739c419541fd1
Fyodor <mlgswagdab@gmail.com>
2023-09-24 18:59:07 +0300

    Добавление функционала

--- src/client/components/pop-ups/AddLecturePopUp.tsx
+++ src/client/components/pop-ups/AddLecturePopUp.tsx
@@ -1,6 +1,9 @@
 import { Modal, Input, Button, Box, Typography } from "@mui/material";
 import { AiOutlineClose } from "react-icons/ai";
 import Select from "react-select";
+import { useState } from "react";
+import axios from "axios";
+
 
 interface AddLecturePopUpProps {
   open: boolean;
@@ -9,60 +12,92 @@ interface AddLecturePopUpProps {
 
 function AddLecturePopUp({ open, setOpen }: AddLecturePopUpProps) {
   const handleClose = () => setOpen(0);
+  const [groupsId, setGroupsId] = useState([]);
+  const [weekday, setWeekday] = useState("");
+  const [time, setTime] = useState("");
+  const [subjectId, setSubjectId] = useState(0);
+  const [place, setPlace] = useState("");
+  const [frequency, setFrequency] = useState("");
+  const [lecturerLogin, setLecturerLogin] = useState("");
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-  const subjects = [
-    { value: "ТП", label: "ТП" },
-    { value: "ИПС", label: "ИПС" },
-    { value: "ИСиС", label: "ИСиС" },
-  ];
+  async function handleSubmit() {
+    try{
+      await axios.post("lessons/create", { groups_id : groupsId, week_day: weekday, time, subject_id: subjectId, place, frequency, professor_login: lecturerLogin });
+      setSuccessMessage("Успех");
+      console.log("Успех");
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        if (error.response.status === 400) {
+          setError("Неверные данные");
+        }
+      }
+    }
+  }
 
-  const lecturers = [
-    { value: "Преподаватель 1", label: "Преподаватель 1" },
-    { value: "Преподаватель 2", label: "Преподаватель 2" },
-    { value: "Преподаватель 3", label: "Преподаватель 3" },
-  ];
+  function handleIds(e : any) {
+    e = e.split(/[ ,]+/);
+    var arr = e.map(Number);
+    return arr;
+  }
 
-  const groups = [
-    { value: "1", label: "1" },
-    { value: "2", label: "2" },
-    { value: "3", label: "3" },
+  const frequencyOptions = [
+    { value: "Числитель", label: "Числитель" },
+    { value: "Знаменатель", label: "Знаменатель" },
   ];
 
   return (
     <Modal open={open} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-      <Box className="m-auto w-1/2 flex flex-col border border-grey px-16 py-8 bg-white translate-y-20">
+      <Box className="m-auto w-1/2 flex flex-col border border-grey px-8 py-2 bg-white translate-y-5">
         <AiOutlineClose cursor={"pointer"} size={20} onClick={handleClose} style={{ alignSelf: "flex-end" }} />
         {/* кнопка закрытия */}
-        <Box className="flex flex-col gap-8 self-center">
+        <Box className="flex flex-col gap-4 self-center">
           <Typography variant="h5" fontWeight="bold">
             Добавить пару
           </Typography>
           <Box>
-            <Typography variant="body1">Выбор дисциплины</Typography>
-            <Select placeholder="Выберите..." options={subjects} className="mt-2" />
-            {/* Выбор дисциплины*/}
+            <Typography variant="body1">Выберите группы</Typography>
+            <Input className="mt-2 p-1" onChange={(e) => setGroupsId(handleIds(e.target.value))}/>
+          </Box>
+          <Box>
+            <Typography variant="body1">Введите ID дисциплины</Typography>
+            <Input type="text" className="mt-2 p-1" fullWidth onChange={(e) => setSubjectId(parseInt(e.target.value))} />
+          </Box>
+          <Box>
+            <Typography variant="body1">Введите день недели</Typography>
+            <Input type="text" className="mt-2 p-1" fullWidth onChange={(e) => setWeekday(e.target.value)} />
+            {/* Ввод дня недели*/}
           </Box>
           <Box>
             <Typography variant="body1">Введите время</Typography>
-            <Input type="text" className="mt-2 p-1" fullWidth />
+            <Input type="text" className="mt-2 p-1" fullWidth onChange={(e) => setTime(e.target.value)} />
             {/* Ввод времени*/}
           </Box>
           <Box>
             <Typography variant="body1">Введите аудиторию</Typography>
-            <Input type="text" className="mt-2 p-1" fullWidth />
+            <Input type="text" className="mt-2 p-1" fullWidth onChange={(e) => setPlace(e.target.value)}/>
             {/* Ввод аудитории*/}
           </Box>
           <Box>
-            <Typography variant="body1">Выберите преподавателя</Typography>
-            <Select placeholder="Выберите..." options={lecturers} className="mt-2" />
-            {/* Выбор преподавателей*/}
+            <Typography variant="body1">Выберите отчётность</Typography>
+            <Select placeholder="Выберите..." options={frequencyOptions} className="mt-2" onChange={(e) => {
+              if (e !== null) {
+                setFrequency(e.value)
+                }}}/>
           </Box>
           <Box>
-            <Typography variant="body1">Выберите группы</Typography>
-            <Select placeholder="Выберите..." isMulti options={groups} className="mt-2" />
-            {/* Выбор групп*/}
+            <Typography variant="body1">Выберите преподавателя</Typography>
+            <Input type="text" className="mt-2 p-1" fullWidth onChange={(e) => setLecturerLogin(e.target.value)}/>
+            {/* Выбор преподавателя*/}
           </Box>
-          <Button variant="contained" size="medium" className="px-2 py-1">
+          <Typography variant="body1" color={"green"}>
+              {successMessage}
+          </Typography>
+          <Typography variant="body1" color={"red"}>
+              {error}
+          </Typography>
+          <Button variant="contained" size="medium" className="px-2 py-1" onClick={handleSubmit}>
             Добавить
           </Button>
         </Box>
